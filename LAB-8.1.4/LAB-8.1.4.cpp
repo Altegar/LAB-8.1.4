9a385ab3119e58b4ebc278e5ea0cc8a72376a623
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2022-03-30 17:39:03 +0300

    Добавьте файлы проекта.

---
+++ LAB-8.1.4/LAB-8.1.4.cpp
@@ -0,0 +1,40 @@
+﻿// LAB-8.1.4.cpp
+// Сушинський Ігор
+// Лабораторна робота №8.1
+// Пошук та заміна символів у літерному рядку.
+// Варіант 20
+// 1 завдання
+// Використання стандартних функцій для рядків string
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Count(const string s)
+{
+	int k = 0;
+	size_t pos = 0;
+	while ((pos = s.find(',', pos)) != -1)
+	{
+		pos++;
+		if (s[pos + 1] == '-')
+			k++;
+	}
+
+	return k;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+
+	string str;
+
+	cout << "Ввести лiтерний рядок:" << endl;
+	getline(cin, str);
+
+	cout << "Лiтерний рядок мiстить " << Count(str) << " групи ', -'" << endl;
+
+	return 0;
+}
